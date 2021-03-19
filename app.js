f2877a54eae74ac46995622b467059235e86df64
DIKIH ARIF WIBOWO <dikih.wibowo@students.amikom.ac.id>
2021-03-19 11:59:31 +0700

    update

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-$(function () {
-    AOS.init();
-});
 $('#humberger').click(function () {
-
     // If the clicked element has the rotate class, remove the rotate class from EVERY .panel-title>.state element
     if ($('#mobile').hasClass('hidden')) {
         $('#mobile').removeClass('hidden');
@@ -10,20 +6,4 @@ $('#humberger').click(function () {
         $('#mobile').addClass('hidden');
     }
 });
-$('.greeting').click(function () {
-    // If the clicked element has the rotate class, remove the rotate class from EVERY .panel-title>.state element
-    if ($('#modal').hasClass('hidden')) {
-        $('#modal').removeClass('hidden');
-    } else {
-        $('#modal').addClass('hidden');
-    }
-});
 
-new Vue({
-    el: "#app",
-    data() {
-        return {
-            name: "World!"
-        };
-    }
-});
